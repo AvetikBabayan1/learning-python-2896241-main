ab6bd75733d75004612def8fb9ffa83f579253f1
Avetik Babayan <avetik.babayan@gmail.com>
2022-09-12 07:34:25 +0400

    Updated with more actions and comments

--- Ch4 - Dates and Times/dates_start.py
+++ Ch4 - Dates and Times/dates_start.py
@@ -3,26 +3,35 @@
 # LinkedIn Learning Python course by Joe Marini
 #
 
+from datetime import date
+from datetime import time
+from datetime import datetime
 
 
 def main():
-    ## DATE OBJECTS
+    # DATE OBJECTS
     # TODO: Get today's date from the simple today() method from the date class
+    today = date.today()
 
-
+    print(today)
     # TODO: print out the date's individual components
+    print(today.day, today.month, today.year)
 
     
     # TODO: retrieve today's weekday (0=Monday, 6=Sunday)
+    days=["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"] #create a list of assigned days, as the weekday function returns a number from 0 to 6
+    print(days[today.weekday()])
 
-    
     ## DATETIME OBJECTS
     # TODO: Get today's date from the datetime class
+    dateToday=datetime.now()
+    print(dateToday)
 
     
     # TODO: Get the current time
-
- 
+    #need to get time portion from the previous result
+    t = datetime.time(datetime.now()) #get the time from datetime.now method
+    print(t)
 
   
 if __name__ == "__main__":
