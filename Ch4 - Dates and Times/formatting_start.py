da3c264f32be5ab6617314d26406167913344c96
Avetik Babayan <avetik.babayan@gmail.com>
2022-09-17 03:17:59 +0400

    Updated with more actions and comments

--- Ch4 - Dates and Times/formatting_start.py
+++ Ch4 - Dates and Times/formatting_start.py
@@ -7,22 +7,27 @@
 from datetime import datetime
 
 def main():
+    now = datetime.now()
+
     # Times and dates can be formatted using a set of predefined string
     # control codes 
 
     
     #### Date Formatting ####
-    
+    print(now.strftime("%Y"))
     # %y/%Y - Year, %a/%A - weekday, %b/%B - month, %d - day of month
-
+    print(now.strftime("%Y, %a, %B, %d"))
 
     # %c - locale's date and time, %x - locale's date, %X - locale's time
-
+    print(now.strftime("%X"))
 
     #### Time Formatting ####
     
     # %I/%H - 12/24 Hour, %M - minute, %S - second, %p - locale's AM/PM
-  
+    print(now.strftime("%I"))
+    print(now.strftime("%H"))
+    print(now.strftime("%p"))
+    print(now.strftime("%H %M %S"))
 
 if __name__ == "__main__":
     main()
--- Ch4 - Dates and Times/timedeltas_start.py
+++ Ch4 - Dates and Times/timedeltas_start.py
@@ -2,34 +2,35 @@
 # Example file for working with timedelta objects
 # LinkedIn Learning Python course by Joe Marini
 #
-
-
+#
+#
 from datetime import date
 from datetime import time
 from datetime import datetime
-
-
-# TODO: construct a basic timedelta and print it
-
-
-# TODO: print today's date
-
-
-# TODO: print today's date one year from now
-
-
-# TODO: create a timedelta that uses more than one argument
-
-
-# TODO: calculate the date 1 week ago, formatted as a string
-
+from datetime import timedelta
+#
+# # TODO: construct a basic timedelta and print it
+# print(timedelta(days=3, hours = 4, minutes=6, seconds=10))
+#
+# # TODO: print today's date
+now = datetime.now()
+# print(now)
+# # TODO: print today's date one year from now
+# print(str(now+timedelta(days=365)))
+#
+# # TODO: create a timedelta that uses more than one argument
+# print(str(now+timedelta(days=3, minutes=24)))
+#
+# # TODO: calculate the date 1 week ago, formatted as a string
+# print(str(now-timedelta(weeks=1)))
 
 ### How many days until April Fools' Day?
-
-
+today=date.today()
+afd=date(today.year, 4, 1)
 # TODO: use date comparison to see if April Fool's has already gone for this year
 # if it has, use the replace() function to get the date for next year
-
+if afd <today:
+    afd=afd.replace(today.year+1)
 
 # TODO: Now calculate the amount of time until April Fool's Day  
-
+print(afd-today)
