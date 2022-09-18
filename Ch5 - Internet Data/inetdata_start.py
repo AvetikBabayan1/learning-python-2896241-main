2e21d65c7e41702cf005b06b34a34980f0f61176
Avetik Babayan <avetik.babayan@gmail.com>
2022-09-18 21:21:05 +0400

    Updated with more actions and comments

--- Ch5 - Internet Data/inetdata_start.py
+++ Ch5 - Internet Data/inetdata_start.py
@@ -3,8 +3,14 @@
 # LinkedIn Learning Python course by Joe Marini
 #
 
+import urllib.request
+import urllib.response
+
 def main():
-    pass # this is a placeholder, do-nothing statement
+    weburl=urllib.request.urlopen("http://espn.com")
+    print(weburl.getcode())
+    data = weburl.read()
+    print(data)
 
 if __name__ == "__main__":
     main()
