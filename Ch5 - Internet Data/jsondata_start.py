8cb6a4e8b2c47928bc33e8d6c038e19163a9bbd3
Avetik Babayan <avetik.babayan@gmail.com>
2022-09-27 07:09:54 +0400

    Updated with more actions and comments

--- Ch5 - Internet Data/jsondata_start.py
+++ Ch5 - Internet Data/jsondata_start.py
@@ -3,26 +3,49 @@
 # LinkedIn Learning Python course by Joe Marini
 #
 
-import urllib.request 
+import urllib.request
+import json
 
 def printResults(data):
     # Use the json module to load the string data into a dictionary
     theJSON = json.loads(data)
+
     
     # now we can access the contents of the JSON like any other Python object
-
+    if "title" in theJSON["metadata"]:
+        print(theJSON["metadata"]["title"])
     
     # output the number of events, plus the magnitude and each event name  
-
+    count=theJSON["metadata"]["count"]
+    print(count)
     
     # for each event, print the place where it occurred
+    #numerical=1
+    #maxmag=0
+    #for places in theJSON["features"]:
+    #    print(numerical,":", places["properties"]["place"], places["properties"]["mag"])
+    #    numerical+=1
+    #    if (places["properties"]["mag"]) > maxmag:
+    #        maxmag=places["properties"]["mag"]
 
+
+    #print("----------------\n")
+    #print("maximal magnitude was: " +str(maxmag))
 
     # print the events that only have a magnitude greater than 4
-
-
+   # maxmag = 0
+   # for places in theJSON["features"]:
+   #     if (places["properties"]["mag"]) >=4.0:
+   #         print(places["properties"]["place"], places["properties"]["mag"])
+   #     if (places["properties"]["mag"]) > maxmag:
+   #         maxmag=places["properties"]["mag"]
+   # print("maximal magnitude was: " + str(maxmag))
     # print only the events where at least 1 person reported feeling something
-
+    for i in theJSON["features"]:
+        feltReport=i["properties"]["felt"]
+        if feltReport != None:
+            if feltReport > 0:
+                print(i["properties"]["place"], i["properties"]["mag"], "felt by ", i["properties"]["felt"])
   
 def main():
     # define a variable to hold the source URL
@@ -33,6 +56,11 @@ def main():
     # Open the URL and read the data
     webUrl = urllib.request.urlopen(urlData)
     print ("result code: " + str(webUrl.getcode()))
+    if (webUrl.getcode() == 200):
+        data = webUrl.read()
+        printResults(data)
+    else:
+        print("Error is received: ", webUrl.result)
   
 
 if __name__ == "__main__":
